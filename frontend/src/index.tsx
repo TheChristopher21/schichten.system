7ded67ad8ce9c682ce6b5cd856928aa7566857ed
TheChristopher21 <siefert.sc@gmail.com>
2024-01-12 16:32:01 +0100

    Merge remote-tracking branch 'origin/main'

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ShiftDetails from "./ShiftDetails";
 import Register from "./Register";
 import Login from "./Login";
 import Header from "./components/Header"
@@ -19,7 +18,6 @@ root.render(
     <BrowserRouter>
       <Routes>
           <Route index element={<Start />} />
-          <Route path="/shift/:id" element={<ShiftDetails />} />
           <Route path="Login" element={<Login />} />
           <Route path="Register" element={<Register />} />
           <Route path="Start" element={<Start />} />
